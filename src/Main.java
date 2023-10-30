aa23eabf67aef36ce92b7437ef0cbb4cfe1a260d
Turcu11 <iosif.turcu@emanuel.ro>
2023-10-30 18:13:32 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         FileDataSource fileDataSource = new FileDataSource();
-        fileDataSource.write("test.txt","Couchi couchi :))");
+        fileDataSource.write("test.txt","Aici este text de proba sa vedem ca scrie in fisier");
         fileDataSource.read("D:\\Facultate\\Anul 3\\Semestrul I\\ingineria-software\\teme\\Decorator\\Decorator\\test.txt");
 
         EncryptionDecorator encd = new EncryptionDecorator(fileDataSource);
